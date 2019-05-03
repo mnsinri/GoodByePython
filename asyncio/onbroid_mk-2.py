5b7c8dd712054e925e9a3f6f359d6189d8bdab8a
mnsinri <khj.mkyohei727@gmail.com>
2019-05-03 19:24:27 +0900

    Add a Searching Command

--- asyncio/onbroid_mk-2.py
+++ asyncio/onbroid_mk-2.py
@@ -115,8 +115,8 @@ LANGUAGES = {
     'yi': 'yiddish',
     'yo': 'yoruba',
     'zu': 'zulu',
-    'fil': 'Filipino',
-    'he': 'Hebrew'
+    'fil': 'filipino',
+    'he': 'hebrew'
 }
 
 def get_token(token_file):
@@ -127,17 +127,49 @@ async def is_English(word):
     alphanum = re.compile(r'^[a-zA-Z0-9]+$')
     return alphanum.match(word) is not None
 
+def this_lang_is(langs):
+    if not langs:
+        embed = discord.Embed(
+            title="I don't know what is what.:thinking:",
+            description="Maybe, it is non-existent lang.",
+            color=0x2ee21f
+            )
+    else:
+        description = ''
+        for lang in langs:
+            if lang not in LANGUAGES:
+                lang = [k for k, v in LANGUAGES.items() if v == lang][0]
+            description += f" {lang} : {LANGUAGES[lang]}\r"
+        embed = discord.Embed(
+        title="It is...",
+        description=description,
+        color=0x2ee21f
+        )
+    return embed
+
 def show_lang():
     description = ''
     for key in LANGUAGES:
         description += f" {key} : {LANGUAGES[key]}\r"
-    embed = discord.Embed(title="**Available Languages**",description=description ,color=0x2ee21f)
+    embed = discord.Embed(
+        title="**Available Languages**",
+        description=description,
+        color=0x2ee21f
+        )
     return embed
 
 async def edit_embed(element, meaning, pronunce, data):
-    embed=discord.Embed(title='**->**'+element, description='**'+meaning+'**\r-----------------------------------------', color=0xff80c0)
+    embed=discord.Embed(
+        title='**->**'+element,
+        description='**'+meaning+'**\r-----------------------------------------',
+        color=0xff80c0
+        )
     for i in range(len(data)):
-        embed.add_field(name=f'>>>  類語{i}: '+str(data[i][0])+'\r_'+str(data[i][1])+"_", value='**->** '+str(data[i][2]), inline=True)
+        embed.add_field(
+            name=f'>>>  類語{i}: '+str(data[i][0])+'\r_'+str(data[i][1])+"_",
+            value='**->** '+str(data[i][2]),
+            inline=True
+            )
     embed.set_footer(text='<発音記号 : '+pronunce+'>')
     return embed
 
@@ -149,7 +181,10 @@ async def nothing_came_up(element):
         f"I know how you feel.:sweat_smile:",
         f":eye:  　:eye:      -----------------------\r       :nose:         <   {element}? So what?\r       :lips:             -----------------------"
     ]
-    embed=discord.Embed(title=fuckyou[randint(0,len(fuckyou)-1)], color=0xff80c0)
+    embed=discord.Embed(
+        title=fuckyou[randint(0,len(fuckyou)-1)],
+        color=0xff80c0
+        )
     return embed
 
 async def get_synoym(url):
@@ -179,13 +214,17 @@ async def weblio_trans(element):
 
 async def google_trans(element, dest):
     translate = t.translate(element, dest=dest).text
-    embed = discord.Embed(title='**->** '+element+"  to  "+dest, description=translate, color=16738740)
+    embed = discord.Embed(
+        title='**->** '+element+"  to  "+LANGUAGES[dest],
+        description=translate,
+        color=16738740
+        )
     embed.set_footer(text='<Brought by google translation>')
     return embed
 
 async def trans(contents):
     if len(contents) == 1 and await is_English(contents[0]):
-        element = contents[0]
+        element = contents[0].lower()
         embed = await weblio_trans(element)
     else:
         dest = 'ja'
@@ -205,17 +244,28 @@ async def trans(contents):
 async def on_message(message):
     if message.author == client.user:
         return
-    elif message.content[0] == '*':
+    elif message.content[0] == '*': #translate
         contents = message.content[1:].split()
         embed = await trans(contents)
         await message.channel.send('', embed=embed)
-    elif message.content == '?help':
-        await message.channel.send('', embed=show_lang())
-    elif message.content == '?onbroid':
-        await session.close()
-        if session.closed:
-            print('Aiohttp client session is closed')
-        await client.close()
+    elif message.content[0] == '?':
+        contents = message.content[1:].split()
+        if contents[0] == 'help': #show available languages
+            await message.channel.send('', embed=show_lang())
+        elif contents[0] == 'onbroid': #close client
+            await session.close()
+            if session.closed:
+                print('Aiohttp client session is closed')
+            await client.close()
+        elif contents[0] == 'whats': #tell available languages
+            langs = []
+            contents.remove('whats')
+            contents_lower = [lang.lower() for lang in contents]
+            for i in range(len(contents)):
+                if contents_lower[i] in LANGUAGES or contents_lower[i] in LANGUAGES.values():
+                    langs.append(contents_lower[i])
+            await message.channel.send('', embed=this_lang_is(langs))
+
 
 if __name__ == '__main__':
     client.run(get_token(token_file))
